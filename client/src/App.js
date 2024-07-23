38449dc9c53f5f72ab57177d40b6b006384db366
W.G.I.K.Umayange <wgikumayanga@gmail.com>
2024-07-24 05:10:41 +0530

    background fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
 import axios from "axios";
 import {useEffect} from "react"
-
+import{ BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Employeeinfo from "./Pages/Employeeinfo/Employeeinfo";
+import Home from "./Pages/home/home";
 function App() {
 
-  useEffect(()=>{
-    axios.get("http://localhost:3001/Users").then ((users)=>{
-      console.log("Users",users.data);
-
-    });
-
-  },[]);
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path='/employeeinfo' exact Component={Employeeinfo}/>
+          <Route path='/home' exact Component={Home}/>
+
+        </Routes>
+      </Router>
     </div>
   );
 }
