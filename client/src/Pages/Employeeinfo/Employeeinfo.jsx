ed4414e1ae5b3e458a5193b380e725ba328f38bc
Umayange1998 <wgikumayanga@gmail.com>
2025-01-31 01:18:10 +0530

    New_functions_In_Employee_table

--- client/src/Pages/Employeeinfo/Employeeinfo.jsx
+++ client/src/Pages/Employeeinfo/Employeeinfo.jsx
@@ -3,9 +3,25 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import BG from "../../assests/BG.jpg";
 import "./table.css";
-
-
-import { Box, Grid, Paper, TableContainer, TablePagination,  Typography, styled } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import SearchIcon from '@mui/icons-material/Search';
+import {
+  Box,
+  Divider,
+  FormHelperText,
+  Grid,
+  IconButton,
+  Input,
+  InputAdornment,
+  OutlinedInput,
+  Paper,
+  TableContainer,
+  TablePagination,
+  TextField,
+  Typography,
+  styled,
+} from "@mui/material";
+import palette from "../../theme/palette";
 function createData(name, code, population, size) {
   const density = population / size;
   return { name, code, population, size, density };
@@ -26,23 +42,23 @@ const MainDiv = styled("div")({
 
 function Employeeinfo() {
   const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
   ];
-  
+
   const [listofUsers, setlistofUsers] = useState([]);
 
   useEffect(() => {
@@ -67,16 +83,13 @@ function Employeeinfo() {
   return (
     <MainDiv>
       <Grid container>
-        
-        <Grid item xs={0.5} sm={1.5} md={2.5} lg={3.5}>
-      
-        </Grid>
+        <Grid item xs={0.5} sm={1.5} md={2.5} lg={4}></Grid>
         <Grid
           item
           xs={11}
           sm={9}
           md={7}
-          lg={5}
+          lg={4}
           sx={{
             backgroundColor: "white",
             padding: {
@@ -87,77 +100,118 @@ function Employeeinfo() {
             width: "100%",
           }}
         >
-          
+          <Box position={"relative"}>
+            <Typography
+              fontWeight={"bold"}
+              sx={{
+                fontSize: {
+                  xs: "18px",
+                  md: "25px",
+                },
+              }}
+              textAlign={"center"}
+            >
+              Employee
+            </Typography>
+            <IconButton
+              sx={{
+                position: "absolute",
+                top: 0,
+                right: 0,
+                borderRadius: 2,
+                fontSize: {
+                  xs: "10px",
+                  md: "12px",
+                },
+
+                px: {
+                  xs: 0.9,
+                  md: 1.5,
+                },
+                backgroundColor: palette.light.primary.main,
+                "&:hover": {
+                  backgroundColor: palette.light.primary.dark,
+                },
+                color: "white",
+              }}
+              variant="contained"
+              onClick={() => {
+                // navigate("/");
+              }}
+            >
+              <AddIcon />
+              Add Employee
+            </IconButton>
+          </Box>
           <Box
             className="empinfo"
             sx={{
               maxHeight: {
                 xs: "500px",
-                md: "300px",
+                md: "400px",
               },
               overflow: "auto",
               width: "100%",
             }}
           >
-            <Typography
-                            fontWeight={"bold"}
-                            sx={{
-                                fontSize: {
-                                    xs: "18px",
-                                    md: "25px",
-                                },
-                            }}
-                            textAlign={"center"}
-                        >
-                      Employee 
-
-        </Typography>
-      <table sx={{ minWidth: 650 }} aria-label="simple table">
-        <thead>
-          <tr>
-            <th>Dessert (100g serving)</th>
-            <th align="right">Calories</th>
-            <th align="right">Fat&nbsp;(g)</th>
-            <th align="right">Carbs&nbsp;(g)</th>
-            <th align="right">Protein&nbsp;(g)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((row) => (
-            <tr
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <td component="th" scope="row">
-                {row.name}
-              </td>
-              <td align="right">{row.calories}</td>
-              <td align="right">{row.fat}</td>
-              <td align="right">{row.carbs}</td>
-              <td align="right">{row.protein}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+            <table sx={{ minWidth: 650 }} aria-label="simple table">
+              <thead>
+                <tr>
+                  <th>Dessert (100g serving)</th>
+                  <th align="right">Calories</th>
+                  <th align="right">Fat&nbsp;(g)</th>
+                  <th align="right">Carbs&nbsp;(g)</th>
+                  <th align="right">Protein&nbsp;(g)</th>
+                </tr>
+              </thead>
+              <tbody>
+                {rows.map((row) => (
+                  <tr
+                    key={row.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <td component="th" scope="row">
+                      {row.name}
+                    </td>
+                    <td align="right">{row.calories}</td>
+                    <td align="right">{row.fat}</td>
+                    <td align="right">{row.carbs}</td>
+                    <td align="right">{row.protein}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </Box>
-          <Box
+          <Box 
             sx={{
-              overflow: "auto",
+              mt: 2,
               width: "100%",
               display: "flex",
               justifyContent: "center",
             }}
           >
-            <TablePagination
-              sx={{ width: "100%" }}
-              rowsPerPageOptions={[10, 25, 100]}
-              component="div"
-              count={listofUsers.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
+            <TextField
+              id="outlined-multiline-flexible"
+              placeholder="Search Employee"
+              multiline
+              maxRows={2}
+              size="small"
+              sx={{
+                width: "200px", // Adjust width as needed
+                borderRadius: "12px", // Customize border radius
+                "& .MuiOutlinedInput-root": {
+                  borderRadius: "12px", // Apply border radius to the input field
+                },
+              }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="end">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
             />
+            
           </Box>
         </Grid>
         <Grid item sx></Grid>
